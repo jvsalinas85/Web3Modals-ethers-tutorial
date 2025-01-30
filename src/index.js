f92d15a502fa26eebf61a2eb65a71e0bafd809fe
Jesus Valencia <jvsalinas85@gmail.com>
2025-01-29 19:08:25 -0600

    ethers appkit and config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { createAppKit } from '@reown/appkit/react'
+import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+import { arbitrum, mainnet } from '@reown/appkit/networks'
+
+// 1. Get projectId
+const projectId = 'YOUR_PROJECT_ID'
+
+// 2. Set the networks
+const networks = [arbitrum, mainnet]
+
+// 3. Create a metadata object - optional
+const metadata = {
+  name: 'My Website',
+  description: 'My Website description',
+  url: 'https://mywebsite.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.mywebsite.com/']
+}
+
+// 4. Create a AppKit instance
+createAppKit({
+  adapters: [new EthersAdapter()],
+  networks,
+  metadata,
+  projectId,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
