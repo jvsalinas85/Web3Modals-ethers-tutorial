a71ac5e619b3338f2424cb145c5c18508d7a17da
Jesus Valencia <jvsalinas85@gmail.com>
2025-02-03 15:18:30 -0600

    added verify message component

--- src/components/VerifyMessageComponent.js
+++ src/components/VerifyMessageComponent.js
@@ -1,6 +1,47 @@
+import { Button, Flex, Input, Text } from "@chakra-ui/react"
+import { ethers } from "ethers";
+import { useState } from "react";
+
 const VerifyMessageComponent = () => {
+
+    const [dataToVerify, setDataToVerify] = useState({
+        message: "",
+        address: "",
+        signature: "",
+    });
+
+    const [isVerified, setIsVerified] = useState(false);
+
+    const onHandleChange = (e) => {
+        const param = e.target.name;
+        const value = e.target.value;
+        setDataToVerify({
+           ...dataToVerify,
+            [param]: value,
+        });
+    };
+
+    const handleVerifyMessage = () => {
+        if (dataToVerify.address && dataToVerify.message && dataToVerify.signature){
+            const recoveredAddress = ethers.verifyMessage(dataToVerify.message, dataToVerify.signature); //returns the private key that produced the signature
+            const isVerified = recoveredAddress === dataToVerify.address;
+            setIsVerified(isVerified);
+        }
+    };
+
   return (
-    <div>VerifyMessageComponent</div>
+    <Flex w={"50%"} gap={2} direction={"column"} p={4}>
+            <Text fontSize={"xl"} color={"white"} fontWeight={"bold"}>Verify Message</Text>
+            <Input placeholder={"Message..."} name="message" onChange={onHandleChange} color={"white"} />
+            <Input placeholder={"Public Key..."} name="address" onChange={onHandleChange} color={"white"} />
+            <Input placeholder={"Signature..."} name="signature" onChange={onHandleChange} color={"white"} />
+            <Button onClick={handleVerifyMessage}>Verify Message</Button>
+            {isVerified && (
+                <Text color={"white"} fontWeight={"bold"}>Message is verified</Text>
+            )};
+            
+            
+        </Flex>
   )
 }
 
